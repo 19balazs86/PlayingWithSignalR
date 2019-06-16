52e4a6a10a25356243ddf9b33f5570a05f016259
Balázs Keresztúri <19balazs86@gmail.com>
2019-06-16 20:52:35 +0100

    Prepare the integration test with helper classes.

---
+++ IntegrationTest/IntegrationTestBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.AspNetCore.TestHost;
+using Xunit;
+
+namespace IntegrationTest
+{
+  public class IntegrationTestBase : IClassFixture<WebApiFactory>
+  {
+    private readonly WebApiFactory _factory;
+
+    protected HttpClient _httpClient => _factory.HttpClient;
+
+    protected TestServer _testServer => _factory.Server;
+
+    public IntegrationTestBase(WebApiFactory factory)
+    {
+      _factory = factory;
+    }
+
+    protected virtual async Task<HubConnection> getHubConnectionAsync(string hubName)
+    {
+      HubConnection hubConnection = new HubConnectionBuilder()
+        .WithUrl($"http://localhost/{hubName}", options =>
+          options.HttpMessageHandlerFactory = _ => _testServer.CreateHandler())
+        .Build();
+
+      await hubConnection.StartAsync();
+
+      return hubConnection;
+    }
+  }
+}
---
+++ IntegrationTest/WebApiFactory.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using PlayingWithSignalR;
+
+namespace IntegrationTest
+{
+  public class WebApiFactory : WebApplicationFactory<Startup>
+  {
+    public HttpClient HttpClient { get; private set; }
+
+    public WebApiFactory()
+    {
+      // Need to create a client for the Server property to be available.
+      HttpClient = CreateClient();
+    }
+
+    protected override IWebHostBuilder CreateWebHostBuilder()
+    {
+      return WebHost
+        .CreateDefaultBuilder()
+        .UseStartup<Startup>();
+    }
+  }
+}

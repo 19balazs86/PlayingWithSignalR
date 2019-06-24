55d6ce5c59193b1e052439363f3320db14acfaa4
Balázs Keresztúri <19balazs86@gmail.com>
2019-06-24 18:57:25 +0100

    Use CountdownEvent.

--- IntegrationTest/HubIntegrationTest.cs
+++ IntegrationTest/HubIntegrationTest.cs
@@ -1,4 +1,5 @@
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
 using PlayingWithSignalR.Hubs;
@@ -19,20 +20,27 @@ namespace IntegrationTest
     public async Task SendMessageToAll()
     {
       // Arrange
-      int counter = 0;
+      CountdownEvent countdownEvent = new CountdownEvent(2);
       Message receivedMessage = null;
 
       HubConnection connection1 = await getHubConnectionAsync(TestUsers.User1);
       HubConnection connection2 = await getHubConnectionAsync(TestUsers.User2);
 
-      connection1.On<Message>(nameof(IMessageClient.ReceiveMessage), msg => receivedMessage = msg);
-      connection2.On<Message>(nameof(IMessageClient.ReceiveMessage), _ => counter++); // +1
+      connection1.On<Message>(nameof(IMessageClient.ReceiveMessage), msg =>
+      {
+        receivedMessage = msg;
+        countdownEvent.Signal();
+      });
+
+      connection2.On<Message>(nameof(IMessageClient.ReceiveMessage), _ => countdownEvent.Signal());
 
       // Act
       await connection1.InvokeAsync(nameof(IMessageHub.SendMessageToAll), _messageToSend);
 
+      countdownEvent.Wait(1000);
+
       // Assert
-      Assert.Equal(1, counter);
+      Assert.Equal(0, countdownEvent.CurrentCount);
       Assert.NotNull(receivedMessage);
       Assert.Equal(_messageToSend, receivedMessage.Text);
       Assert.Equal(TestUsers.User1.Id, receivedMessage.UserId);
@@ -77,21 +85,27 @@ namespace IntegrationTest
 
       Notification notification = new Notification { Message = _messageToSend };
 
-      int counter = 0;
+      CountdownEvent countdownEvent = new CountdownEvent(2);
       Message receivedMessage = null;
 
       HubConnection connection1 = await getHubConnectionAsync(TestUsers.User1);
       HubConnection connection2 = await getHubConnectionAsync(TestUsers.User2);
 
-      connection1.On<Message>(nameof(IMessageClient.ReceiveMessage), _ => counter++); // +1
-      connection2.On<Message>(nameof(IMessageClient.ReceiveMessage), msg => receivedMessage = msg);
+      connection1.On<Message>(nameof(IMessageClient.ReceiveMessage), _ => countdownEvent.Signal());
+      connection2.On<Message>(nameof(IMessageClient.ReceiveMessage), msg =>
+      {
+        receivedMessage = msg;
+        countdownEvent.Signal();
+      });
 
       // Act: User1 initiates an HTTP call to send a notification for everyone.
       HttpResponseMessage response = await _httpClient.PostAsJsonAsync("Notification", notification);
 
+      countdownEvent.Wait(1000);
+
       // Assert
       Assert.True(response.IsSuccessStatusCode);
-      Assert.Equal(1, counter);
+      Assert.Equal(0, countdownEvent.CurrentCount);
       Assert.NotNull(receivedMessage);
       Assert.Equal(notification.Message, receivedMessage.Text);
       Assert.Equal(_testUser.Id, receivedMessage.UserId);

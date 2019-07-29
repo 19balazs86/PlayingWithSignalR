1c0384af7c5223e66056b4ec20b588d6a9368456
Balázs Keresztúri <19balazs86@gmail.com>
2019-07-29 21:46:48 +0100

    Change FakeUserFilter to AuthDelegatingHandler.

---
+++ IntegrationTest/AuthDelegatingHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using PlayingWithSignalR.Infrastructure;
+
+namespace IntegrationTest
+{
+  public class AuthDelegatingHandler : DelegatingHandler
+  {
+    private readonly Func<IEnumerable<Claim>> _testUserClaimsFunc;
+
+    public AuthDelegatingHandler(Func<IEnumerable<Claim>> testUserClaimsFunc)
+      => _testUserClaimsFunc = testUserClaimsFunc;
+
+    protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancelToken)
+    {
+      IEnumerable<Claim> claims = _testUserClaimsFunc?.Invoke();
+
+      if (claims != null)
+        request.Headers.Authorization = new AuthenticationHeaderValue(
+          JwtBearerDefaults.AuthenticationScheme, TokenFactory.CreateToken(claims));
+
+      return base.SendAsync(request, cancelToken);
+    }
+  }
+}
--- IntegrationTest/WebApiFactory.cs
+++ IntegrationTest/WebApiFactory.cs
@@ -1,10 +1,5 @@
 ﻿using System.Net.Http;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
 using PlayingWithSignalR;
 using PlayingWithSignalR.Models;
 
@@ -19,23 +14,7 @@ namespace IntegrationTest
     public WebApiFactory()
     {
       // Need to create a client for the Server property to be available.
-      HttpClient = CreateClient();
-    }
-
-    protected override IWebHostBuilder CreateWebHostBuilder()
-    {
-      return WebHost
-        .CreateDefaultBuilder()
-        .ConfigureTestServices(services =>
-        {
-          // This hack does not have any effect on the Hub. Just for the HTTP call.
-          services.AddMvc(options =>
-          {
-            options.Filters.Add(new AllowAnonymousFilter());
-            options.Filters.Add(new FakeUserFilter(() => TestUser?.ToClaims()));
-          });
-        })
-        .UseStartup<Startup>();
+      HttpClient = CreateDefaultClient(new AuthDelegatingHandler(() => TestUser?.ToClaims()));
     }
   }
 }

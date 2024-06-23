ef0f05929003558a3e203090a57d386a7110f60d
Balazs Kereszturi <19balazs86@gmail.com>
2024-06-23 21:02:13 +0200

    Add a link in comment about Hub.Context.UserIdentifier

--- PlayingWithSignalR/Hubs/MessageHub.cs
+++ PlayingWithSignalR/Hubs/MessageHub.cs
@@ -7,6 +7,9 @@ public sealed class MessageHub : Hub<IMessageClient>, IMessageHub
 {
     public const string Path = "/hub/messages";
 
+    // https://learn.microsoft.com/en-us/aspnet/core/signalr/hubs#the-context-object
+    // https://learn.microsoft.com/en-us/aspnet/core/signalr/authn-and-authz?#use-claims-to-customize-identity-handling
+    // SignalR uses the ClaimTypes.NameIdentifier from the ClaimsPrincipal
     private Guid _userIdentifier => Guid.Parse(Context.UserIdentifier);
 
     public override Task OnConnectedAsync()
